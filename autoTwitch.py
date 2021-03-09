450a5ef56513b835b8d29b337299887040b80716
JosephCantrell <39743346+JosephCantrell@users.noreply.github.com>
2021-03-09 09:58:19 -0600

    Update to the main code
    Made the search for the Live element as well as the game element a function. Rewrote the order in which the program searches for the game to make the code run faster. Essentially made the code faster and prettier

--- autoTwitch.py
+++ autoTwitch.py
@@ -104,69 +104,91 @@ class AutoStream:
                         name = links.split(".tv/")
                         startTime = ''
                         
-                        isPlaying = True
+                        foundLive = False
+                        isPlayingGame = False
+                        isPlaying = False
+                        
                         # Find the "Live" element.
-                        temp = self.browser.find_elements_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div/div[1]/a/div/div/div/div[2]/div/div/div/p')
-                        if not temp:
-                                print(name[1] + " Is not live currently. Moving on")
-                                noError = False
-                        # Find the game element
-                        temp = self.browser.find_elements_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div[1]/a/span')                
-                        if not temp:            
-                            print(name[1] + " Is not playing " + self.args.game)
-                            noError = False
-                        elif temp[0].text.lower() != str(self.args.game.lower()):
-                            print(name[1] + " Is not playing " + self.args.game + ". They are playing " + temp[0].text.lower())
-                            noError = False
-                            
-                            
-                        isPlaying = True
+                        foundLive = self.findLiveElement()
                         
-                        
-                        if noError:
+                        # Only do the next search if the streamer is Live
+                        if foundLive:
+                            
+                            isPlayingGame = self.findGameElement()
+
+
+                        if foundLive and isPlayingGame:
                             print(name[1] + ' Is Live and playing ' + self.args.game)
                             startTime = datetime.datetime.now()
-                        while isPlaying and noError:
-                            
-                            time.sleep(30)
-                            
-                            
-                            # Randomly refresh the page.
-                            if random.randint(0,50) == 45:
-                                self.browser.get(streamerURL[i])
-                                time.sleep(10)
-                            
-                            temp = self.browser.find_elements_by_class_name("tw-strong tw-upcase tw-white-space-nowrap")
-                            
-                            if temp:
-                                if temp[0].lower() != 'live':          
-                                    print(links + " Is no longer live")
-                                    isPlaying = False
-                            temp = self.browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div[1]/a/span').text.lower()
-                            if temp != str(self.args.game.lower()):
-                                print(links + ' Is no longer playing ' + self.args.game)
-                                isPlaying = False
-                            nowtime = datetime.datetime.now()
-                            timeDelta = nowtime - startTime
-                            totalSeconds = timeDelta.total_seconds()
-                            mins = totalSeconds/60
-                            if int(args.watchTime) != 0:
-                                if mins > int(args.watchTime) + random.randint(0,3):
-                                    print('We have watched for over two hours. Time to move on. Changeing ' + str(i) + ' in status to True')
-                                    personalAlreadyGot[i] = True
-                                    isPlaying = False
-                                    temp = ''
-                                    for item in personalAlreadyGot:
-                                        temp = temp + item + ' '
-                                    file = open('status.txt','w')
-                                    file.write(temp)
-                                    file.close()
-                                else:
-                                    print('Still Watching. Its been %d Minutes' % mins)
+                            isPlaying = True
+                            while isPlaying:
+                                
+                                time.sleep(30)
+                                
+                                
+                                # Randomly refresh the page.
+                                if random.randint(0,50) == 45:
+                                    self.browser.get(streamerURL[i])
+                                    time.sleep(10)
+                                
+                                isPlaying = self.findLiveElement()
+                                
+                                isPlaying = self.findGameElement()
+
+                                nowtime = datetime.datetime.now()
+                                timeDelta = nowtime - startTime
+                                totalSeconds = timeDelta.total_seconds()
+                                mins = totalSeconds/60
+                                if int(args.watchTime) != 0:
+                                    if mins > int(args.watchTime) + random.randint(0,3):
+                                        print('We have watched for over %d minutes. Time to move on. Changeing ' + str(i) + ' in status to True' % args.watchTime)
+                                        personalAlreadyGot[i] = True
+                                        isPlaying = False
+                                        trueCount += 1
+                                        temp = ''
+                                        for item in personalAlreadyGot:
+                                            temp = temp + item + ' '
+                                        file = open('status.txt','w')
+                                        file.write(temp)
+                                        file.close()
+                                    else:
+                                        print('Still Watching. Its been %d Minute(s)' % mins)
+                    else:
+                        print('Already Aquired This Drop. Moving On')
                 time.sleep(60*10)
             else:
                 print('All drops have been reported as counted. Breaking.')
                 break
+
+    def findLiveElement(self):
+        temp = self.browser.find_elements_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div/div/div/div[1]/a/div/div/div/div[2]/div/div/div/p')
+        if not temp:
+                print("Could not find the Live Element (Is Offline). Moving on")
+                return False
+        # Go through the 'Live' elements and search for the text.
+        else:
+            for items in temp:
+                if items.text.lower() == 'live':
+                    foundLive = True
+                    return True
+                return False
+    
+    def findGameElement(self):
+        temp = self.browser.find_elements_by_xpath('//*[@id="root"]/div/div[2]/div/main/div[2]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div[1]/a/span')   
+        # If the streamer is Live and we have found elements in the 
+        if not temp:            
+            print("Could not find the Game Element (Not Playing Anything). Moving On")
+            return False
+        # Cycle through the game elements that we found to find the correct element
+        else:
+            for items in temp:
+                # If the current element has the game title that we are looking for, the streamer is playing the correct game
+                if items.text.lower() == str(self.args.game.lower()):
+                    isPlayingGame = True  
+                    return True
+                return False
+
+        
 
 parser = argparse.ArgumentParser(description='Linkedin Automatic Job Applications')
 parser.add_argument('-g','--game', help='What game should the code check for', required=True,default='')
